a6afbb77c66008f17efc4eda4175ea7983efc5a8
francisco.junior <francisco.junior@lgcns.com>
2021-12-24 16:46:52 -0400

    adicionando validações de tratamento

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUpdatePost;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store(StoreUpdatePost $request)
+    {
+        $post = Post::create($request->all());
+        return redirect()->route('posts.index');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    PostController
+};
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Teste
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostController::class,'index'])->name('posts.index');
+Route::get('/posts/create', [PostController::class,'create'])->name('posts.create');
+Route::post('/posts', [PostController::class,'store'])->name('posts.store');
